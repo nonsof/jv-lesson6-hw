dcb8a0da522c0ae7d2d5afa9eb608f6fdecc89a8
nonsof <113558928+nonsof@users.noreply.github.com>
2023-09-29 13:54:43 +0300

    update

--- main.java
+++ main.java
@@ -1,18 +1,74 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+public class Notebook {
+    private String brand;           // Марка ноутбука (например, "HP", "Dell", "Lenovo" и т.д.)
+    private int ramSizeGB;          // Размер оперативной памяти в ГБ
+    private int storageSizeGB;      // Объем жесткого диска в ГБ
+    private String operatingSystem; // Операционная система (например, "Windows 10", "macOS", "Linux" и т.д.)
+    private String color;           // Цвет ноутбука
+    private double price;           // Цена ноутбука
 
-public class Main {
-    public static void main(String[] args) {
+    // Конструктор класса
+    public Notebook(String brand, int ramSizeGB, int storageSizeGB, String operatingSystem, String color, double price) {
+        this.brand = brand;
+        this.ramSizeGB = ramSizeGB;
+        this.storageSizeGB = storageSizeGB;
+        this.operatingSystem = operatingSystem;
+        this.color = color;
+        this.price = price;
+    }
 
-        Set<Notebook> set = new HashSet<>();
-        set.add(new Notebook("Notebook 1", 8, "Windows10", 80000, "HP"));
-        set.add(new Notebook("Notebook 2", 16, "Windows10", 85000, "Asus"));
-        set.add(new Notebook("Notebook 3", 32, "linux", 85000, "Lenovo"));
-        set.add(new Notebook("Notebook 4", 64, "linux", 90000, "Lenovo"));
+    // Геттеры и сеттеры для полей класса
 
-        OperationWithNotebook operation = new OperationWithNotebook(set);
-        operation.start();
+    public String getBrand() {
+        return brand;
+    }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getRamSizeGB() {
+        return ramSizeGB;
+    }
+
+    public void setRamSizeGB(int ramSizeGB) {
+        this.ramSizeGB = ramSizeGB;
+    }
+
+    public int getStorageSizeGB() {
+        return storageSizeGB;
+    }
+
+    public void setStorageSizeGB(int storageSizeGB) {
+        this.storageSizeGB = storageSizeGB;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Переопределение метода toString() для удобного вывода информации о ноутбуке
+    @Override
+    public String toString() {
+        return "Brand: " + brand + ", RAM: " + ramSizeGB + "GB, Storage: " + storageSizeGB + "GB, OS: " + operatingSystem + ", Color: " + color + ", Price: $" + price;
     }
 }
